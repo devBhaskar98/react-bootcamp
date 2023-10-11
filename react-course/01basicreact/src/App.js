60421318e91448ef404133f4d1ac48716faf2054
Bhaskar Dixit <bhaskardixit786@gmail.com>
2023-10-12 00:07:40 +0530

    youtube app in cra

--- react-course/01basicreact/src/App.js
+++ react-course/01basicreact/src/App.js
@@ -1,7 +1,12 @@
+import Youtube from "./Youtube";
 
 function App() {
   return (
-    <h2>Classic React App</h2>
+    <>
+      <h2>Classic React App</h2>
+      <Youtube/>
+    </>
+    
   );
 }
 
